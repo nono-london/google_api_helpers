7b9d31702a42f9e23a14155a997a25799c5ae022
hplaptop <7720585+nono-london@users.noreply.github.com>
2023-04-20 04:38:27 +0200

    changed from cwd to __file__ version 1.0.3

--- google_api_helpers/app_config.py
+++ google_api_helpers/app_config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 
 def load_env_variables():
-    env_path: Path = Path(get_root_full_path(), ".env")
+    env_path: Path = Path(get_root_full_path().parent, ".env")
     if not env_path:
         print(f"No env file found for path: {env_path}")
         raise Exception(f".env file not found")
@@ -14,14 +14,14 @@ def load_env_variables():
 
 def get_root_full_path() -> Path:
     """Return the app root folder path"""
-    app_root_path: Path = Path().cwd().parent
+    app_root_path: Path = Path(__file__).parent
     return app_root_path
 
 
 def get_g_credentials_path() -> Path:
     """Return the path to the Google credentials folder and create the folder if it doesn't exist"""
     app_root_path: Path = get_root_full_path()
-    credential_path: Path = Path(app_root_path, app_root_path.name, "g_credentials")
+    credential_path: Path = Path(app_root_path, "g_credentials")
     if not (credential_path.exists()):
         credential_path.mkdir(parents=False,  # uses parent False as it shouldn't have to create any parent folders
                               exist_ok=True)
